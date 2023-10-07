aa76e868fa9e63be42d152d3065d2c19a137e460
maybellineboon <maybelline.boon@gmail.com>
2023-10-07 14:50:56 -0700

    update hard coded url and port

--- services/dsp/src/public/js/dsp.js
+++ services/dsp/src/public/js/dsp.js
@@ -1,3 +1,5 @@
-let iframe = document.createElement('iframe');
-iframe.src = "https://privacy-sandbox-demos-dsp.dev/private-aggregation";
-document.body.appendChild(iframe);
+window.addEventListener("load", (event) => {
+    let iframe = document.createElement('iframe');
+    iframe.src = "https://privacy-sandbox-demos-dsp.dev/private-aggregation";
+    document.body.appendChild(iframe);
+  });
