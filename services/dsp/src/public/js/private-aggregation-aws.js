222eee7113de0b4fb83a505d9d5c9376de2bf7f1
Maybelline Boon <maybellineb@google.com>
2024-02-13 20:41:49 -0800

    update package.json with cbor since dsp index.ts requires cbor.

--- services/dsp/src/public/js/private-aggregation-aws.js
+++ services/dsp/src/public/js/private-aggregation-aws.js
@@ -1,7 +1,7 @@
 async function runPrivateAggregationAws() {
   const privateAggCloud = {'privateAggregationConfig':
     {'aggregationCoordinatorOrigin': 'https://publickeyservice.msmt.aws.privacysandboxservices.com'}};
-  await window.sharedStorage.worklet.addModule('js/private-aggregation-aws-worklet.js');
+  await window.sharedStorage.worklet.addModule('js/private-aggregation-worklet-aws.js');
   await window.sharedStorage.run('test-private-aggregation', privateAggCloud);
 }
 
--- services/dsp/src/public/js/private-aggregation-gcp.js
+++ services/dsp/src/public/js/private-aggregation-gcp.js
@@ -1,7 +1,7 @@
 async function runPrivateAggregationGcp() {
     const privateAggCloud = {'privateAggregationConfig':
       {'aggregationCoordinatorOrigin': 'https://publickeyservice.msmt.gcp.privacysandboxservices.com'}};
-    await window.sharedStorage.worklet.addModule('js/private-aggregation-gcp-worklet.js');
+    await window.sharedStorage.worklet.addModule('js/private-aggregation-worklet-gcp.js');
     await window.sharedStorage.run('test-private-aggregation', privateAggCloud);
   }
   
