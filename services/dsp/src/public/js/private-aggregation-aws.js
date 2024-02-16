f45dde36b7d0b388e3567ab1b5c015749a9f381e
Maybelline Boon <maybellineb@google.com>
2024-02-16 13:02:59 -0800

    update with pre-commit

--- services/dsp/src/public/js/private-aggregation-aws.js
+++ services/dsp/src/public/js/private-aggregation-aws.js
@@ -1,7 +1,13 @@
 async function runPrivateAggregationAws() {
-  const privateAggCloud = {'privateAggregationConfig':
-    {'aggregationCoordinatorOrigin': 'https://publickeyservice.msmt.aws.privacysandboxservices.com'}};
-  await window.sharedStorage.worklet.addModule('js/private-aggregation-worklet-aws.js');
+  const privateAggCloud = {
+    'privateAggregationConfig': {
+      'aggregationCoordinatorOrigin':
+        'https://publickeyservice.msmt.aws.privacysandboxservices.com',
+    },
+  };
+  await window.sharedStorage.worklet.addModule(
+    'js/private-aggregation-worklet-aws.js',
+  );
   await window.sharedStorage.run('test-private-aggregation', privateAggCloud);
 }
 
--- services/dsp/src/public/js/private-aggregation-gcp.js
+++ services/dsp/src/public/js/private-aggregation-gcp.js
@@ -1,9 +1,14 @@
 async function runPrivateAggregationGcp() {
-    const privateAggCloud = {'privateAggregationConfig':
-      {'aggregationCoordinatorOrigin': 'https://publickeyservice.msmt.gcp.privacysandboxservices.com'}};
-    await window.sharedStorage.worklet.addModule('js/private-aggregation-worklet-gcp.js');
-    await window.sharedStorage.run('test-private-aggregation', privateAggCloud);
-  }
-  
-  runPrivateAggregationGcp();
-  
+  const privateAggCloud = {
+    'privateAggregationConfig': {
+      'aggregationCoordinatorOrigin':
+        'https://publickeyservice.msmt.gcp.privacysandboxservices.com',
+    },
+  };
+  await window.sharedStorage.worklet.addModule(
+    'js/private-aggregation-worklet-gcp.js',
+  );
+  await window.sharedStorage.run('test-private-aggregation', privateAggCloud);
+}
+
+runPrivateAggregationGcp();
