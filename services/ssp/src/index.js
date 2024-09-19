a5942ba069a16dc6a6b447cfa3e1db24c19f9c52
Sid Sahoo <siddharth.s.sahoo@gmail.com>
2024-09-19 20:44:57 +0000

    Update comment.

--- services/ssp/src/index.js
+++ services/ssp/src/index.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  // Enable transitional debug reports
+  // Enable transitional debug reports for ARA.
   res.cookie('ar_debug', '1', {
     sameSite: 'none',
     secure: true,
