3cd7fc687d048609bb59d464ba28b5600c23c7a3
Sid Sahoo <siddharth.s.sahoo@gmail.com>
2024-10-08 00:49:00 +0000

    Clean up header bidding lib.

--- services/news/src/public/js/test-header-bidding.js
+++ services/news/src/public/js/test-header-bidding.js
@@ -28,17 +28,31 @@
  */
 
 (() => {
+  // ****************************************************************
+  // CONSTANTS
+  // ****************************************************************
   /** Local storage item containing configurations for this library. */
   const LOCAL_STORAGE_HEADER_BIDDING_CONFIG = 'HEADER_BIDDING_LIB_CONFIG';
   /** Local storage item containing configurations for ad units on page. */
   const LOCAL_STORAGE_PAGE_ADS_CONFIG = 'PAGE_ADS_CONFIG';
   /** Local storage item to store the winning header bid. */
   const LOCAL_STORAGE_WINNING_HEADER_BID = 'WINNING_HEADER_BID';
+  /** Local storage item to store component auction configs. */
+  const LOCAL_STORAGE_AUCTION_CONFIGS = 'AUCTION_CONFIGS';
+  /** Local storage item to indicate overall status of header bidding. */
+  const LOCAL_STORAGE_HEADER_BIDDING_STATUS = 'HEADER_BIDDING'
+  /** Text indicating pending status. */
+  const HEADER_BIDDING_STATUS_PENDING = 'PENDING';
+  /** Text indicating completed status. */
+  const HEADER_BIDDING_STATUS_COMPLETE = 'COMPLETE';
   /** Timeout in milliseconds for the on-page contextual auction. */
   const CONTEXTUAL_AUCTION_TIMEOUT_MS = 5000;
 
+  // ****************************************************************
+  // HELPER FUNCTIONS
+  // ****************************************************************
   /** Builds and returns the various bid request URLs. */
-  const getContextualBidRequestUrls = (sellers, adUnits) => {
+  const getBidRequestUrlsWithContext = (sellers, adUnits) => {
     const bidRequestUrls = [];
     for (const adUnit of adUnits) {
       const bidRequestQuery = Object.entries(adUnit)
@@ -57,50 +71,29 @@
     return bidRequestUrls;
   };
 
-  /** Returns a contextual bid response for the given URL. */
-  const getContextualBidResponse = async (bidRequestUrl) => {
-    console.log('[PSDemo] Header bidding lib making contextual bid request', {
-      bidRequestUrl,
-    });
-    const response = await fetch(bidRequestUrl);
-    if (response.ok) {
-      const bidResponse = await response.json();
-      console.log('[PSDemo] Header bidding lib received contextual bid', {
-        bidRequestUrl,
-        bidResponse,
-      });
-      if (bidResponse.componentAuctionConfig) {
-        const key = `AUCTIONCONFIG-${bidResponse.bidder}-${bidResponse.auctionId}`;
-        console.log('[PSDemo] Header bidding lib storing auction config', {
-          key,
-          auctionConfig: bidResponse.componentAuctionConfig,
-        });
-        localStorage.setItem(
-          key,
-          JSON.stringify(bidResponse.componentAuctionConfig),
-        );
-      }
-      return bidResponse;
-    } else {
-      console.log('[PSDemo] Error in contextual bid response', {
-        statusText: response.statusText,
-        bidRequestUrl,
-      });
-      return {
-        bidder,
-        bid: 0.0,
-      };
-    }
-  };
-
   /** Runs and returns contextual bid responses. */
-  const runContextualAuction = async (sellers, adUnits) => {
-    const bidRequestUrls = getContextualBidRequestUrls(sellers, adUnits);
-    const bidResponsePromises = bidRequestUrls.map((bidRequestUrl) =>
-      getContextualBidResponse(bidRequestUrl),
-    );
+  const getAllBidResponses = async (sellers, adUnits) => {
+    const bidRequestUrls = getBidRequestUrlsWithContext(sellers, adUnits);
+    const bidResponsePromises = bidRequestUrls.map(async (bidRequestUrl) => {
+      console.log('[PSDemo] Making header bid request', {bidRequestUrl});
+      const response = await fetch(bidRequestUrl);
+      if (response.ok) {
+        const bidResponse = await response.json();
+        console.log('[PSDemo] Received header bid response', {bidResponse});
+        return bidResponse;
+      } else {
+        console.log('[PSDemo] Error in contextual bid response', {
+          statusText: response.statusText,
+          bidRequestUrl,
+        });
+        return {bid: 0.0};
+      }
+    });
+    // Use Promise.race to implement the timeout.
     const bidResponses = await Promise.race([
-      Promise.allSettled(bidResponsePromises),
+      (await Promise.allSettled(bidResponsePromises))
+        .filter((p) => p.status === 'fulfilled')
+        .map((p) => p.value),
       new Promise((resolve) =>
         setTimeout(() => resolve([]), CONTEXTUAL_AUCTION_TIMEOUT_MS),
       ),
@@ -108,33 +101,121 @@
     return bidResponses;
   };
 
-  /** Main function that initializes the header bidding library. */
-  (async () => {
-    try {
-      console.log('[PSDemo] Initializing header bidding library.');
-      /** Read library configuration from local storage. */
-      const {sellers} = JSON.parse(
-        localStorage.getItem(LOCAL_STORAGE_HEADER_BIDDING_CONFIG),
-      );
-      /** Read configurations for ad units on page. */
-      const {adUnits} = JSON.parse(
-        localStorage.getItem(LOCAL_STORAGE_PAGE_ADS_CONFIG),
-      );
-      /** Start contextual auction. */
-      console.log('[PSDemo] Header bidding library initialized.');
-      const contextualBids = await runContextualAuction(sellers, adUnits);
-      const [winningContextualBid] = contextualBids.sort(
-        (bid1, bid2) => bid1.bid - bid2.bid,
-      );
-      localStorage.setItem(
-        LOCAL_STORAGE_WINNING_HEADER_BID,
-        JSON.stringify(winningContextualBid),
-      );
-    } catch (err) {
-      console.log('[PSDemo] Header bidding library could not be initialized.', {
-        err,
-      });
-      throw err;
+  /** Sorts bid responses by auction ID and extracts auction configs. */
+  const getBidResponsesAndAuctionConfigs = (auctionIds, bidResponses) => {
+    const bidResponsesByAuctionId = {};
+    const auctionConfigsByAuctionId = {};
+    for (const bidResponse of bidResponses) {
+      if (!auctionIds.includes(bidResponse.auctionId)) {
+        console.log('Ignoring bid response for unexpected auctionId', {
+          auctionIds,
+          bidResponse,
+        });
+        continue;
+      }
+      const {auctionId, componentAuctionConfig} = bidResponse;
+      if (!bidResponsesByAuctionId.hasOwnProperty(auctionId)) {
+        bidResponsesByAuctionId[auctionId] = [];
+      }
+      bidResponsesByAuctionId[auctionId].push(bidResponse);
+      if (componentAuctionConfig) {
+        if (!auctionConfigsByAuctionId.hasOwnProperty(auctionId)) {
+          auctionConfigsByAuctionId[auctionId] = [];
+        }
+        auctionConfigsByAuctionId[auctionId].push(componentAuctionConfig);
+      }
     }
+    return [bidResponsesByAuctionId, auctionConfigsByAuctionId];
+  };
+
+  /** Stores the winning bid and auction configs for each auction IDs. */
+  const writeWinningBidsAndAuctionConfigsToLocalStorage = (
+    auctionIds,
+    bidResponsesByAuctionId,
+    auctionConfigsByAuctionId,
+  ) => {
+    for (const auctionId of auctionIds) {
+      const bidResponses = bidResponsesByAuctionId[auctionId];
+      if (!bidResponses) {
+        console.log('[PSDemo] No header bids received', {auctionId});
+        continue;
+      }
+      const [winningBid] = bidResponses.sort(
+        (bid1, bid2) => Number(bid2.bid) - Number(bid1.bid),
+      );
+      const keyForWinningBid = `${LOCAL_STORAGE_WINNING_HEADER_BID}||${auctionId}`;
+      localStorage.setItem(
+        keyForWinningBid,
+        JSON.stringify(winningBid),
+      );
+      console.log('[PSDemo] Winning header bid written to local storage', {
+        auctionId,
+        localStorageKey: keyForWinningBid,
+        winningBid,
+      });
+      if (auctionConfigsByAuctionId.hasOwnProperty(auctionId)) {
+        const auctionConfigs = auctionConfigsByAuctionId[auctionId];
+        const keyForAuctionConfigs = `${LOCAL_STORAGE_AUCTION_CONFIGS}||${auctionId}`;
+        localStorage.setItem(
+          keyForAuctionConfigs,
+          JSON.stringify(auctionConfigs),
+        );
+        console.log(
+          '[PSDemo] Header bidding lib stored auction configs', {
+            auctionId,
+            localStorageKey: keyForAuctionConfigs,
+            auctionConfigs,
+        });
+      }
+    }
+  };
+
+  /** Cleans older output configurations from local storage. */
+  const resetLocalStorage = () => {
+    const allKeys = Object.keys(localStorage);
+    const keysToDel = [];
+    // Remove keys for old winning header bids.
+    keysToDel.push(...allKeys.filter(key => key.startsWith(LOCAL_STORAGE_WINNING_HEADER_BID)));
+    // Remove keys for old auction configs.
+    keysToDel.push(...allKeys.filter(key => key.startsWith(LOCAL_STORAGE_AUCTION_CONFIGS)));
+    console.log('[PSDemo] Removing old header bidding outputs', {keysToDel});
+    for (const keyToRemove of keysToDel) {
+      localStorage.removeItem(keyToRemove);
+    }
+  }
+
+  // ****************************************************************
+  // MAIN FUNCTION
+  // ****************************************************************
+  (async () => {
+    // Set status to PENDING while header bidding is in progress.
+    localStorage.setItem(LOCAL_STORAGE_HEADER_BIDDING_STATUS,
+      HEADER_BIDDING_STATUS_PENDING);
+    resetLocalStorage();
+    // Read configurations from local storage.
+    const {sellers} = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_HEADER_BIDDING_CONFIG),
+    );
+    const {adUnits} = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_PAGE_ADS_CONFIG),
+    );
+    console.log('[PSDemo] Header bidding library initialized', {
+      sellers,
+      adUnits,
+    });
+    // Start contextual auction.
+    const auctionIds = adUnits.map((adUnit) => adUnit.auctionId);
+    const contextualBidResponses = await getAllBidResponses(sellers, adUnits);
+    const [bidResponsesByAuctionId, auctionConfigsByAuctionId] =
+      getBidResponsesAndAuctionConfigs(auctionIds, contextualBidResponses);
+    // Write winning bid and auction configs to local storage.
+    writeWinningBidsAndAuctionConfigsToLocalStorage(
+      auctionIds,
+      bidResponsesByAuctionId,
+      auctionConfigsByAuctionId,
+    );
+    // Set status to COMPLETE.
+    localStorage.setItem(LOCAL_STORAGE_HEADER_BIDDING_STATUS,
+      HEADER_BIDDING_STATUS_COMPLETE);
   })();
 })();
