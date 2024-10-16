711ff5ab52cbeddf6b8b428558e6549306581d8b
Sid Sahoo <siddharth.s.sahoo@gmail.com>
2024-10-16 18:45:54 +0000

    Fix reference to current script

--- services/ssp/src/public/js/video-ad-tag.js
+++ services/ssp/src/public/js/video-ad-tag.js
@@ -16,7 +16,7 @@
 
 (async () => {
   const ins = document.querySelector('ins.ads');
-  const script = document.querySelector('.ssp_tag');
+  const script = document.currentScript;
   const src = new URL(script.src);
   src.pathname = '/video-ad-tag.html';
   const iframe = document.createElement('iframe');
