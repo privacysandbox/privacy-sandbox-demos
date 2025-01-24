2ae0e3a161f7cf42731856c2b19b1f03a4315e66
Syna <synakim@google.com>
2025-01-24 12:22:54 +0900

    Update services/ad-tech/src/lib/arapi.ts
    Co-authored-by: code-review-assist[bot] <182814678+code-review-assist[bot]@users.noreply.github.com>

--- services/ad-tech/src/lib/arapi.ts
+++ services/ad-tech/src/lib/arapi.ts
@@ -231,7 +231,7 @@ function test() {
 
 // tmp for event level report
 // assign trigger data and priority based on the conversion type
-export function getTriggerData(cvType: String) {
+export function getTriggerData(cvType: string) {
   console.log('[arapi] getTriggerData: ', cvType);
   switch (cvType) {
     case 'click-cart-icon':
