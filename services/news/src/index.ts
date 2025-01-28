72cffe175f28e68388179ec98fcad7bfeeb09576
syna <sinnews@hotmail.com>
2025-01-28 07:57:08 +0000

    replace hard coded url with constants

--- services/news/src/index.ts
+++ services/news/src/index.ts
@@ -28,6 +28,7 @@ import {
   SSP_B_ORIGIN,
   SSP_HOST,
   SSP_ORIGIN,
+  SHOP_HOST,
 } from './constants.js';
 
 const app: Application = express();
@@ -51,6 +52,7 @@ app.get('*', async (req: Request, res: Response) => {
   res.render(req.path.substring(1), {
     TITLE: NEWS_DETAIL,
     TEXT_LOREM,
+    SHOP_HOST,
     AD_SERVER_HOST,
     DSP_HOST,
     EXTERNAL_PORT,
