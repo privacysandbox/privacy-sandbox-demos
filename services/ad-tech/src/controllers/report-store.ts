4153f4dc9e6337760e45e140daa6bc7e49b5ebd0
cwserna <cwserna@google.com>
2025-03-14 22:51:49 +0000

    Adding a route to clear report cache for demo and debugging

--- services/ad-tech/src/controllers/report-store.ts
+++ services/ad-tech/src/controllers/report-store.ts
@@ -36,8 +36,8 @@ export interface Report {
   data: any;
 }
 
-/** TTL for in-memory reports: 10 minutes */
-export const REPORT_TTL_SECONDS = 10 * 60;
+/** TTL for in-memory reports: 30 minutes */
+export const REPORT_TTL_SECONDS = 30 * 60;
 
 /** Simple in-memory implementation of report storage. */
 export const ReportStore = (() => {
@@ -55,8 +55,16 @@ export const ReportStore = (() => {
     return Reports.keys().map((key) => Reports.get(key) as Report);
   };
 
+  /** Delete all reports from in-memory storage. */
+  const cleanReports = (): Report[] => {
+    Reports.flushAll();
+    console.log('keys flushed');
+    return Reports.keys().map((key) => Reports.get(key) as Report);
+  };
+
   return {
     addReport,
     getAllReports,
+    cleanReports,
   };
 })();
