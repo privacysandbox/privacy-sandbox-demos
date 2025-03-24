4cfd71724c9cfd8820142f1b4a56e984990f1696
Kasper Kulikowski <kaskulikowski@google.com>
2025-03-24 08:49:02 +0000

    ExamplePay checkout available on item view and adding auto redirect

---
+++ services/shop/src/public/js/fencedframe.js
@@ -0,0 +1,45 @@
+function createFencedFrame() {
+  //   let cartLength = <%=cart.length%>;
+
+  //   if (cartLength === 0) {
+  //     return;
+  //   }
+
+  let fencedframe = document.createElement('fencedframe');
+
+  fencedframe.addEventListener('fencedtreeclick', () => {
+    let height = 500;
+    let width = 600;
+    let left = screen.width / 2 - width / 2;
+    let top = screen.height / 2 - height / 2;
+    let params =
+      'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top;
+
+    window.open(
+      'https://privacy-sandbox-demos-services.dev/popup',
+      'example-pay-popup',
+      params,
+    );
+
+    window.setTimeout(function () {
+      window.location.href = 'https://privacy-sandbox-demos-shop.dev/checkout';
+    }, 3000);
+  });
+
+  try {
+    fencedframe.config = new FencedFrameConfig(
+      'https://privacy-sandbox-demos-services.dev/button',
+    );
+
+    fencedframe.height = '75px';
+    fencedframe.width = '250px';
+    fencedframe.style.border = '0px';
+    fencedframe.style.borderRadius = '25px';
+
+    document.getElementById('button-holder').appendChild(fencedframe);
+  } catch (e) {
+    console.log('Cannot provide personalized button.');
+  }
+}
+
+createFencedFrame();
